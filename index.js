9b31c29508adae1edecce60205e95e2de8d27a73
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-02 15:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function add2(n) {
 
 var funkyFunction = function outer() {
   return function inner() {
-    return 'FUNKY!'
+    return "FUNKY!"
   }
-  //inner();
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
